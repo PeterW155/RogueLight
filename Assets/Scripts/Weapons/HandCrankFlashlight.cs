9a724a0eb9b9799a56f21f512e91d7489f6a1dfd
Ben Tang <sjps30503@gmail.com>
2023-01-29 20:12:28 -0500

    Damage box visual

--- Assets/Scripts/Weapons/HandCrankFlashlight.cs
+++ Assets/Scripts/Weapons/HandCrankFlashlight.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class HandCrankFlashlight : Weapon
 {
+    [SerializeField] private GameObject _damageBoxVisual;
+    
     public override void FireDown()
     {
         if (DamageBox)
             DamageBox.enabled = true;
+        if (_damageBoxVisual)
+            _damageBoxVisual.SetActive(true);
     }
 
     public override void FireUp()
     {
         if (DamageBox)
             DamageBox.enabled = false;
+        if (_damageBoxVisual)
+            _damageBoxVisual.SetActive(false);
     }
 }
