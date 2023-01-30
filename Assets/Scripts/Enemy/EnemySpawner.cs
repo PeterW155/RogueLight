f8dfd492a8492d9f7f2dea96cdbaf6aa320d2060
Peter155 <110631086+PeterW155@users.noreply.github.com>
2023-01-29 20:59:48 -0500

    Enemy Spawner
    Enemy spawner, 4/10 for zombie, 4/10 for ghost, 2/10 for brute

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public float maxTime;
+    public float timer;
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+
+        if(timer <= 0.0)
+        {
+            SpawnEnemy();
+            timer = maxTime;
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        int rand = (int)Random.Range(0, 10);
+        if(rand <= 4)
+        {
+            Instantiate(enemies[0], this.transform);
+        }
+        else if(rand <= 8)
+        {
+            Instantiate(enemies[1], this.transform);
+        }
+        else if (rand <= 10)
+        {
+            Instantiate(enemies[2], this.transform);
+        }
+    }
+}
