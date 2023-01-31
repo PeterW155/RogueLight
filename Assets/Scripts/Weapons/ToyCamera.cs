db902ae45e5b57d1847e9ea038d80551209af211
Ben Tang <sjps30503@gmail.com>
2023-01-30 22:02:40 -0500

    ToyCamera attack pattern

---
+++ Assets/Scripts/Weapons/ToyCamera.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyCamera : Weapon
+{
+    [SerializeField] private float _activationDuration;
+    [SerializeField] private float _cooldownDuration;
+
+    private float _lastActivationTime;
+
+    private void Start()
+    {
+        // Activation Duration should be shorter than Cooldown Duration
+        _activationDuration = Mathf.Min(_activationDuration, _cooldownDuration);
+    }
+
+    public override void FireDown()
+    {
+        if (Time.time < _lastActivationTime + _cooldownDuration)
+            return;
+
+        if (CurrentRound < 0)
+            return;
+        
+        CurrentRound--;
+        _lastActivationTime = Time.time;
+        
+        if (DamageBox)
+            DamageBox.enabled = true;
+        if (_damageBoxVisual)
+            _damageBoxVisual.SetActive(true);
+
+        StartCoroutine(TurnOff());
+    }
+
+    private IEnumerator TurnOff()
+    {
+        yield return new WaitForSeconds(_activationDuration);
+        if (DamageBox)
+            DamageBox.enabled = false;
+        if (_damageBoxVisual)
+            _damageBoxVisual.SetActive(false);
+    }
+
+    public override void FireUp() { }
+}
