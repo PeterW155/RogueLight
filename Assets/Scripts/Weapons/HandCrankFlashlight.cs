ae1db6941356b8d61eba955e1b187840ebfb9c20
Ben Tang <sjps30503@gmail.com>
2023-01-30 21:46:12 -0500

    HandCrankFlashlight attack behavior

--- Assets/Scripts/Weapons/HandCrankFlashlight.cs
+++ Assets/Scripts/Weapons/HandCrankFlashlight.cs
@@ -1,22 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 public class HandCrankFlashlight : Weapon
 {
     [SerializeField] private GameObject _damageBoxVisual;
-    
+    [SerializeField] private float _activationDuration;
+    [SerializeField] private float _cooldownDuration;
+
+    private float _lastActivationTime;
+
+    private void Start()
+    {
+        // Activation Duration should be shorter than Cooldown Duration
+        _activationDuration = Mathf.Min(_activationDuration, _cooldownDuration);
+    }
+
     public override void FireDown()
     {
+        if (Time.time < _lastActivationTime + _cooldownDuration)
+            return;
+
+        _lastActivationTime = Time.time;
+        
         if (DamageBox)
             DamageBox.enabled = true;
         if (_damageBoxVisual)
             _damageBoxVisual.SetActive(true);
+
+        StartCoroutine(TurnOff());
     }
 
-    public override void FireUp()
+    private IEnumerator TurnOff()
     {
+        yield return new WaitForSeconds(_activationDuration);
         if (DamageBox)
             DamageBox.enabled = false;
         if (_damageBoxVisual)
             _damageBoxVisual.SetActive(false);
     }
+
+    public override void FireUp() { }
 }
